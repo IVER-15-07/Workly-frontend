06e55ccb1c542ee00745f790d5cd51a50ec1200a
Soto2401 <alecito2ale@gmail.com>
2025-11-23 22:55:14 -0400

    actualizacion

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://unfacaded-nylah-staid.ngrok-free.dev";
+const URL = import.meta.env.VITE_API_WS || "http://localhost:3000";
 
 export const socket = io(URL, {
   autoConnect: false,
